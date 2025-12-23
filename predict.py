ed175ca4089f40409172c4d1794961ce5cb0b9e5
seungil <s24060@gsm.hs.kr>
2025-12-23 09:59:37 +0900

    feat/반환 값 추가

--- predict.py
+++ predict.py
@@ -5,6 +5,11 @@ from super_gradients import setup_device
 from fastapi import UploadFile, File, APIRouter
 import numpy as np
 
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+import os
+import dotenv
+
 router = APIRouter(prefix="/api/ai", tags=["emotions"])
 
 setup_device(device="cuda")
@@ -23,6 +28,46 @@ CLASS_NAMES = [
         "hurt",
         "sad",
     ]
+
+CLASS_KO = {
+        "happy": "행복",
+        "surprise": "놀람",
+        "anger": "분노",
+        "anxiety": "불안",
+        "hurt": "상처",
+        "sad": "슬픔",
+    }
+
+dotenv.load_dotenv()
+
+chat_model = ChatGoogleGenerativeAI(model="gemini-flash-latest",
+                                   google_api_key=os.getenv("GOOGLE_API_KEY"))
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", """
+        당신은 감정 점수를 해석하여
+        현재 감정 상태를 설명하는 전문가입니다.
+       
+        규칙:
+        - 입력으로 주어지는 감정 점수는 0~1 사이입니다.
+        - 가장 점수가 높은 감정을 중심으로 현재 상태를 해석하세요.
+        - 감정 이름을 반드시 출력에 포함하세요.
+        - 극단적 표현, 위협적인 문장은 절대 사용하지 마세요.
+        - 항상 존댓말을 사용하세요.
+        - 마크다운 사용금지 및 이스케이프문 사용금지
+        - 문장 길이는 100자 내외로 간결하게 작성하세요.
+        - 한글만 사용
+        - 감정은 한글만으로 표현
+          
+        **무조건 지켜야할 출력 형식 예시**:
+          오늘은 전반적으로 긍정적인 감정 상태입니다. 행복함이 주된 감정으로 나타났으며, 평온한 상태를 유지하고 있습니다.
+
+    """),
+
+    ("human", "{emotion_scores}"),
+])
+
+chain = prompt | chat_model
 
 @router.post("/emotions")
 async def emotion_detection(image: UploadFile = File(...)):
@@ -59,4 +104,15 @@ async def emotion_detection(image: UploadFile = File(...)):
             k: 0.0 for k in CLASS_NAMES
         }
 
-    return emotion_scores
+    resp = chain.invoke({"emotion_scores": emotion_scores})
+
+    try:
+      content = resp.content[0]["text"]
+    except Exception as e:
+      content = resp.content
+
+    return {
+        "emotions": emotion_scores,
+        "emotion" : CLASS_KO[max(emotion_scores, key=emotion_scores.get)],
+        "comment" : content
+    }
